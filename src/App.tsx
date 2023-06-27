23c384b7869a765d7ab6027ed2ec68f25f0833f3
Ilya Maksymov <ivashthestampede01@gmail.com>
2023-06-27 18:55:52 +0300

    fix: Reslove conflict with modal booking

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -9,43 +9,27 @@ import * as theme from './theme';
 import { Login } from './pages/Login';
 import { Signup } from './pages/Signup';
 import { Home } from './pages/Home';
-<<<<<<< HEAD
-import { ModalBooking } from './components/ModalBooking';
-
-const App: FC = () => {
-  return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <ThemeProvider theme={theme}>
-        <Styled.GlobalStyles />
-        <Login />
-        {/* <Signup /> */}
-        {/* <ItemCardSmall /> */}
-        {/* <ItemCard /> */}
-        {/* <Home /> */}
-        <ModalBooking cafeName="Very Well Cafe" />
-      </ThemeProvider>
-    </LocalizationProvider>
-=======
 import { appRoutes } from './routes/Routes';
 import { Cafe } from './pages/Cafe';
 import { ScrollToTop } from './helpers/ScrollToTop';
 
 const App: FC = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Styled.GlobalStyles />
-      <BrowserRouter>
-        <ScrollToTop>
-          <Routes>
-            <Route path={appRoutes.home} element={<Home />} />
-            <Route path={appRoutes.login} element={<Login />} />
-            <Route path={appRoutes.register} element={<Signup />} />
-            <Route path={appRoutes.cafes} element={<Cafe />} />
-          </Routes>
-        </ScrollToTop>
-      </BrowserRouter>
-    </ThemeProvider>
->>>>>>> main
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <ThemeProvider theme={theme}>
+        <Styled.GlobalStyles />
+        <BrowserRouter>
+          <ScrollToTop>
+            <Routes>
+              <Route path={appRoutes.home} element={<Home />} />
+              <Route path={appRoutes.login} element={<Login />} />
+              <Route path={appRoutes.register} element={<Signup />} />
+              <Route path={appRoutes.cafes} element={<Cafe />} />
+            </Routes>
+          </ScrollToTop>
+        </BrowserRouter>
+      </ThemeProvider>
+    </LocalizationProvider>
   );
 };
 
