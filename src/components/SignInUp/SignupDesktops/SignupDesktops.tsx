5386ba1aa5ef3b9b2425e1e23ce2a4e9332c0d67
Artem Nasadchuk <nasadchuk007@gmail.com>
2023-07-07 00:48:31 +0300

    feature: added validation for input fields

--- src/components/SignInUp/SignupDesktops/SignupDesktops.tsx
+++ src/components/SignInUp/SignupDesktops/SignupDesktops.tsx
@@ -1,5 +1,8 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 import { Input } from '../../Input';
 import { Button } from '../../Button';
@@ -14,8 +17,64 @@ import {
   ContentImage,
 } from '../CommonContent';
 import { appRoutes } from '../../../routes/Routes';
+import styled from 'styled-components';
+
+export const StyledError = styled.p`
+  color: red;
+  font-size: 13px;
+`
+
+const schema = yup.object().shape({
+  email: yup.string().required().email('invalid email'),
+  firstname: yup.string().required().min(4).max(60),
+  lastname: yup.string().required().min(4).max(60),
+  password: yup
+  .string()
+  .required()
+  .min(8, 'password must be at least 8 characters')
+  .max(30, 'password must not exceed 30 characters')
+  .matches(
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/,
+    'password must contain at least one digit, one lowercase letter, and one uppercase letter'
+  ),
+  passwordRepeat: yup
+  .string()
+  .oneOf([yup.ref('password'), undefined], 'passwords must match')
+  .required('repeat your password is a required field'),
+});
+
+type FormData = {
+  email: string;
+  firstname: string;
+  lastname: string;
+  password: string;
+  passwordRepeat: string;
+};
 
 export const SignupDesktops: FC = () => {
+  const [formData, setFormData] = useState<FormData>({
+    email: '',
+    firstname: '',
+    lastname: '',
+    password: '',
+    passwordRepeat: '',
+  });
+
+  const [isCreated, setIsCreated] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onBlur",
+  });
+
+  const onSubmit = (data: FormData) => {
+    console.log('Form data:', data);
+  };
+
   return (
     <ContentDesktopsStyled>
       <ContentWrapper>
@@ -30,33 +89,53 @@ export const SignupDesktops: FC = () => {
           </p>
         </div>
         <div>
-          <Form>
-            <FormContainer>
-              <Input
-                type="email"
-                label="Enter your email"
-                name="email"
-                placeholder="m.brown@gmail.com"
-              />
-              <Input
-                type="text"
-                label="Enter your name"
-                name="name"
-                placeholder="Marry Brown"
-              />
-              <Input
-                type="password"
-                label="Create your password (min 8 chars)"
-                name="password"
-              />
-              <Input
-                type="password"
-                label="Repeat your password"
-                name="password-repeat"
-              />
-            </FormContainer>
-            <Button type="submit">Sign up</Button>
-          </Form>
+          {isCreated ? (
+            <h1>Account created</h1>
+          ) : (
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <FormContainer>
+                <Input
+                  type="email"
+                  name="email"
+                  label="Enter your email"
+                  placeholder="m.brown@gmail.com"
+                  register={register('email')}
+                />
+                {errors.email && <StyledError>{errors.email.message}</StyledError>}
+                <Input
+                  type="text"
+                  name="firstname"
+                  label="Enter your firstname"
+                  placeholder="Marry"
+                  register={register('firstname')}
+                />
+                {errors.firstname && <StyledError>{errors.firstname.message}</StyledError>}
+                <Input
+                  type="text"
+                  name="lastname"
+                  label="Enter your lastname"
+                  placeholder="Marry"
+                  register={register('lastname')}
+                />
+                {errors.lastname && <StyledError>{errors.lastname.message}</StyledError>}
+                <Input
+                  type="password"
+                  name="password"
+                  label="Create your password (min 8 chars)"
+                  register={register('password')}
+                />
+                <p>{errors.password && <StyledError>{errors.password.message}</StyledError>}</p>
+                <Input
+                  type="password"
+                  name="passwordRepeat"
+                  label="Repeat your password"
+                  register={register('passwordRepeat')}
+                />
+                <p>{errors.passwordRepeat && <StyledError>{errors.passwordRepeat.message}</StyledError>}</p>
+              </FormContainer>
+              <Button type="submit">Sign up</Button>
+            </Form>
+          )}
         </div>
       </ContentWrapper>
       <ContentImage></ContentImage>
